8863990572671e68b4bc2e087e27a3510e7fc033
Burak Miraç Bol <burakmiracbol46@gmail.com>
2026-01-09 18:43:39 +0300

    bazı kod dosyaları içi lisasns başlığı eklendi

--- lib/sheets/somestatefullwidgetskeleton.dart
+++ lib/sheets/somestatefullwidgetskeleton.dart
@@ -1,3 +1,18 @@
+// Copyright (C) 2026  [Burak Miraç Bol]
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 
 class StatefulWidgetSkeletonSheet extends StatefulWidget {
@@ -15,3 +30,4 @@ class _StatefulWidgetSkeletonSheetState
     return Container();
   }
 }
+// By the way this file is for just to fasten up to developing progress and it has no effects on project
