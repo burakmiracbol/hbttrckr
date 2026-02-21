a0a2fe65f0848ca836b3c3c791609f0defa5cbea
burakmiracbol <burakmiracbol46@gmail.com>
2026-02-21 14:34:35 +0300

    liquid_wrapper.dart for easily use liquid glass when needed

---
+++ lib/classes/liquid_wrapper.dart
@@ -0,0 +1,44 @@
+// hbttrckr: just a habit tracker
+// Copyright (C) 2026  Burak Miraç Bol
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'package:conditional_wrap/conditional_wrap.dart';
+import 'package:flutter/material.dart';
+import 'package:liquid_glass_renderer/liquid_glass_renderer.dart';
+
+class LiquidWrapper extends StatelessWidget {
+  final Widget child;
+  final bool statement;
+  final LiquidShape shape;
+  const LiquidWrapper({
+    required this.child,
+    required this.statement,
+    required this.shape,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetWrapper(
+      wrapper: (child) => statement
+          ? LiquidGlass(
+        shape: shape,
+        child: GlassGlow(child: child),
+      )
+          : child,
+      child: child,
+    );
+  }
+}
